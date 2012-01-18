6128a886467d619245507cb81593fa36ca4e63f9
Robert Norrie <8294241+robertnorrie@users.noreply.github.com>
2012-01-18 18:36:57 +0000

    Add methods for accessing and updating the feature catalog properties

--- qgis/featureCatCreator/iso19110.py
+++ qgis/featureCatCreator/iso19110.py
@@ -12,19 +12,27 @@ from PyQt4 import QtXml
 from PyQt4 import QtCore
 
 import os.path
+import xml.etree.ElementTree as etree
 
 def generateXML(templatePath, param):
-    # Is the template path an URL or a file system path?
+    templateContent = getTemplateContent(templatePath)
+    isoDoc = iso19110Doc(templateContent)
 
-    # Testing if the path is empty
+    return isoDoc.toString()
+
+
+def getTemplateContent(templatePath):
+    # is the template path an URL or a file system path?
+
+    # testing if the path is empty
     if templatePath.trimmed().isEmpty():
         raise Exception, "The text field specifying the template path is empty."
         
-    # Testing if the path is a file system path
+    # testing if the path is a file system path
     if os.path.exists(templatePath):
         return getTemplateContentFromFileSystemPath(templatePath)
     
-    # Testing if the URL is valid
+    # testing if the URL is valid
     else:
         qUrl = QtCore.QUrl(templatePath)
         if not qUrl.isValid():
@@ -36,7 +44,7 @@ def generateXML(templatePath, param):
 
 
 def getTemplateContentFromUrl(templateUrl):
-    # Retrieving the content of the file
+    # retrieving the content of the file
     import urllib2
     return urllib2.urlopen(templateUrl).read()
 
@@ -54,20 +62,117 @@ def getTemplateContentFromFileSystemPath(templateFileSystemPath):
 
     
 class iso19110Doc:
+    
+    def __init__(self, templateContent):
+        self.templateContent = templateContent.decode('utf-8')
+        #print templateContent
+        #self.templateContent = "sdfsfsdf"
+        self.checkTemplateValidity()
+        
+        # using QtXml
+        self.domDoc = QtXml.QDomDocument()
+        self.domDoc.setContent(QtCore.QString(self.templateContent))
+        
+        # using etree
+        self.etDoc = etree.fromstring(self.templateContent)
+    
+    def checkTemplateValidity(self):
+        templateDomDoc = QtXml.QDomDocument()
+        
+        # check if the template is a valid XML document
+        domParsingResult =  templateDomDoc.setContent(QtCore.QString(self.templateContent))
+        if not domParsingResult[0]:
+            raise Exception, "The selected iso19110 template is not a valid XML document (%s at line %d and column %d)" % (domParsingResult[1], domParsingResult[2], domParsingResult[3])
+        
+        # check the declaration of namespaces
+        
+        # check the presence and cardinality of required elements
+        # gfc:FC_FeatureCatalogue
+        
+        # gfc:FC_FeatureCatalogue/gfc:producer
+        
+        pass
+    
+    def toString(self):
+        #return self.domDoc.toString(2)
+        return etree.tostring(self.etDoc)
 
-    def __init__(self, templateContent, param):
+    def updateDomWithParam(self, param):
+        # update the feature catalogue name, scope and version number
+        
+        # update the feature  type name and definition
+        
+        # update the attributes
+            
+            # update the type, definition and cardinality of the attribute
+            
+            # update the listed values of the attribute
+        
         pass
-        
-    def getContent(self):
-        return u""
-        
-    def updateFcName(self, newName):
+
+
+    def getFcName(self):
+        fcName = self.etDoc.findall("./{%s}name/{%s}CharacterString" % (GFC_NS, GCO_NS))[0].text
+        return fcName
+
+    def setFcName(self, newName):
+        fcNames = self.etDoc.findall("./{%s}name/{%s}CharacterString" % (GFC_NS, GCO_NS))
+        if len(fcNames) != 1:
+            self.cleanFcName()
+            
+        fcNames[0].text = newName
+
+    def cleanFcName(self):
         pass
-        
-    def updateFcScope(self, newScope):
+
+   
+    def getFcScope(self):
+        fcScope = self.etDoc.findall("./{%s}scope/{%s}CharacterString" % (GFC_NS, GCO_NS))[0].text
+        return fcScope
+
+    def setFcScope(self, newScope):
+        fcScopes = self.etDoc.findall("./{%s}scope/{%s}CharacterString" % (GFC_NS, GCO_NS))
+        if len(fcScopes) != 1:
+            self.cleanFcScope()
+            
+        fcScopes[0].text = newScope
+
+    def cleanFcScope(self):
         pass
-        
-    def updateFcVersionNumber(self, newVersionNumber):
+
+
+    def getFcVersionNumber(self):
+        fcVersion = self.etDoc.findall("./{%s}versionNumber/{%s}CharacterString" % (GFC_NS, GCO_NS))[0].text
+        return fcVersion
+
+    def setFcVersionNumber(self, newVersionNumber):
+        fcVersions = self.etDoc.findall("./{%s}versionNumber/{%s}CharacterString" % (GFC_NS, GCO_NS))
+        if len(fcVersions) != 1:
+            self.cleanFcVersionNumber()
+            
+        fcVersions[0].text = newVersionNumber
+
+    def cleanFcVersionNumber(self):
         pass
+
+
+    def updateFcVersionDate(self):
+        fcDates = self.etDoc.findall("./{%s}versionDate" % (GFC_NS))
+        if len(fcDates) != 1:
+            self.cleanFcVersionDate()
         
+        # remove all child elements
+        dateValues = fcDates[0].getchildren()
+        for dateValue in dateValues:
+            fcDates[0].remove(dateValue)
+            
+        # set the new value
+        import datetime
+        today = datetime.date.today()
+        format = "%Y-%m-%d"        
+        newDateNode = etree.SubElement(fcDates[0], "{%s}%s" % (GCO_NS, "Date"))
+        newDateNode.text = today.strftime(format)
         
+    def cleanFcVersionDate(self):
+        pass
+
