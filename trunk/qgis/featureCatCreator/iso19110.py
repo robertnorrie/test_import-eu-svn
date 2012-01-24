c1a1d517ffb20fb68da3ac6e86d60bd0e752ebc3
picavvin <picavvin@6ea4d1b0-2fa9-4423-a4f2-244cad39d1e9>
2012-01-24 17:52:44 +0000

    Licence and references cleanup

--- trunk/qgis/featureCatCreator/iso19110.py
+++ trunk/qgis/featureCatCreator/iso19110.py
@@ -1,4 +1,30 @@
 # -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ featureCatCreator
+                                 A QGIS plugin
+    Generates, for a selected datasource, a feature catalogue metadata
+    record in XML format compliant with ISO19110 standard.
+    This project has been funded by the EEA (European Environment Agency) :
+    http://www.eea.europa.eu
+    The project has been developped by Neogeo and Oslandia :
+    http://www.neogeo-online.net/
+    http://www.oslandia.com
+                             -------------------
+        begin                : 2012-01-17
+        copyright            : (C) 2012 Neogeo & Oslandia, funded by EEA
+        email                : vincent.picavet@oslandia.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 
 # This is a module dedicated to generating ISO19110 XML data
 # given specific parameters.
